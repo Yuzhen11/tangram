4a1f30c0ad14ddb3f8c59e6766d437fec89fd2d1
Yuzhen11 <huangyuzhen93@gmail.com>
2018-03-01 15:23:49 +0800

    add the missing files

---
+++ core/plan/collection_builder_spec.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <sstream>
+
+#include "base/sarray_binstream.hpp"
+
+namespace xyz {
+
+struct CollectionBuilderSpec {
+  int collection_id;
+  int num_partition;
+  SArrayBinStream data;
+
+  std::string DebugString() const {
+    std::stringstream ss;
+    ss << "{ collection_id: " << collection_id;
+    ss << ", num_partition: " << num_partition;
+    ss << ", data size in char: " << data.Size();
+    ss << "}";
+    return ss.str();
+  }
+
+  friend SArrayBinStream& operator<<(xyz::SArrayBinStream& stream, const CollectionBuilderSpec& s) {
+    stream << s.collection_id << s.num_partition << s.data;
+  	return stream;
+  }
+  
+  friend SArrayBinStream& operator>>(xyz::SArrayBinStream& stream, CollectionBuilderSpec& s) {
+    stream >> s.collection_id >> s.num_partition >> s.data;
+  	return stream;
+  }
+};
+
+}  // namespace xyz
+
