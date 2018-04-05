1fc27d224cbfab85c60f07b49d04075dcc4078cb
Yuzhen11 <huangyuzhen93@gmail.com>
2018-04-06 01:21:34 +0800

    add compare_pr, mv pagerank*.cpp to pagerank/

---
+++ examples/pagerank/compare_pr.cpp
@@ -0,0 +1,83 @@
+#include "core/plan/runner.hpp"
+
+#include "boost/tokenizer.hpp"
+
+DEFINE_string(url1, "", "");
+DEFINE_string(url2, "", "");
+
+using namespace xyz;
+
+struct PRpair {
+  int id;
+  float pr1;
+  float pr2;
+
+  using KeyT = int;
+  PRpair() = default;
+  PRpair(int _id) : id(_id) {}
+  KeyT Key() const { return id; }
+};
+
+struct Diff {
+  int id;
+  float diff;
+
+  using KeyT = int;
+  Diff() = default;
+  Diff(int _id) : id(_id) {}
+  KeyT Key() const { return id; }
+};
+
+int main(int argc, char** argv) {
+  Runner::Init(argc, argv);
+  CHECK(FLAGS_url1.size());
+  CHECK(FLAGS_url2.size());
+
+  auto load = [](std::string& s) {
+    std::pair<int, float> p;
+    boost::char_separator<char> sep(" \t");
+    boost::tokenizer<boost::char_separator<char>> tok(s, sep);
+    boost::tokenizer<boost::char_separator<char>>::iterator it = tok.begin();
+    p.first = std::stoi(*it++);
+    p.second = std::stof(*it);
+    return p;
+  };
+  auto pr1 = Context::load(FLAGS_url1, load);
+  auto pr2 = Context::load(FLAGS_url2, load);
+
+  auto pr_pair = Context::placeholder<PRpair>(5);
+
+  Context::mapjoin(pr1, pr_pair, 
+    [](std::pair<int, float> p) {
+      return p;
+    },
+    [](PRpair* p, float a) {
+      p->pr1 = a;
+    });
+
+  Context::mapjoin(pr2, pr_pair, 
+    [](std::pair<int, float> p) {
+      return p;
+    },
+    [](PRpair* p, float a) {
+      p->pr2 = a;
+    });
+
+  auto diff = Context::placeholder<Diff>(1);
+  Context::mapjoin(pr_pair, diff,
+    [](PRpair p) {
+      // LOG(INFO) << "pr1, pr2, diff: " 
+      //   << p.pr1 << " " << p.pr2 << " " << fabs(p.pr2 - p.pr1);
+      return std::make_pair(0, float(fabs(p.pr2 - p.pr1)));
+    },
+    [](Diff* d, float a) {
+      d->diff += a;
+    })
+  ->SetCombine([](float* a, float b) { *a += b; });
+
+  Context::foreach(diff, [](Diff d) {
+    LOG(INFO) << RED("The difference is: " + std::to_string(d.diff));
+  });
+
+  Runner::Run();
+}
---
+++ examples/pagerank/compare_pr.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+from os.path import dirname, realpath 
+
+proj_dir = dirname(dirname(dirname(realpath(__file__))))
+print proj_dir
+sys.path.append(proj_dir+"/scripts/")
+
+from launcher import Launcher
+
+hostfile = "machinefiles/5nodes"
+progfile = "release/ComparePR"
+schedulerfile = "release/SchedulerMain"
+
+common_params = {
+    "scheduler" : "proj10",
+    "scheduler_port" : "33227",
+    "hdfs_namenode" : "proj10",
+    "hdfs_port" : 9000,
+}
+
+program_params = {
+    "url1": "/tmp/tmp/yz/pr",
+    "url2": "/tmp/tmp/yz/pr",
+}
+
+scheduler_params = {
+    "dag_runner_type" : "sequential",
+}
+
+env_params = (
+  "GLOG_logtostderr=true "
+  "GLOG_v=-1 "
+  "GLOG_minloglevel=0 "
+  # this is to enable hdfs short-circuit read (disable the warning info)
+  # change this path accordingly when we use other cluster
+  # the current setting is for proj5-10
+  "LIBHDFS3_CONF=/data/opt/course/hadoop/etc/hadoop/hdfs-site.xml"
+  )
+
+dump_core = False
+l = Launcher(schedulerfile, progfile, hostfile,
+             common_params, scheduler_params, program_params, env_params,
+             dump_core)
+
+l.Launch(sys.argv)
